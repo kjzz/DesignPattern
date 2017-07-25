9fa2493f095af73c2537033b1c28ece2d3f1162d
zhangkejie <kejie.zhang@hyperchain.cn>
2017-07-25 15:10:57 +0800

    feat:add StateMethod.

---
+++ src/StateMethod/Context.java
@@ -0,0 +1,29 @@
+package StateMethod;
+
+/**
+ * Created by Jeremy on 2017/7/25.
+ */
+public class Context {
+
+    private State state;
+
+    public Context(State state){
+        this.state = state;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void method() {
+        if (state.getValue().equals("state1")) {
+            state.method1();
+        } else if (state.getValue().equals("state2")) {
+            state.method2();
+        }
+    }
+}
---
+++ src/StateMethod/StateTest.java
@@ -0,0 +1,21 @@
+package StateMethod;
+
+/**
+ * Created by Jeremy on 2017/7/25.
+ */
+public class StateTest {
+
+    public static void main(String []args){
+        State state = new State();
+        Context context = new Context(state);
+
+        //设置第一种状态
+        state.setValue("state1");
+        context.method();
+
+        //设置第二种状态
+        state.setValue("state2");
+        context.method();
+    }
+
+}
